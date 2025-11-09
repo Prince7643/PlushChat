ef9af0e67ad23c01a620e5729e1785dc7148d431
Prine7643 <princekumar809016@gmail.com>
2025-11-09 21:28:27 +0530

    Change in backend

--- Backend/src/utils/gernateVerificationlink.ts
+++ Backend/src/utils/gernateVerificationlink.ts
@@ -3,7 +3,7 @@ import crypto from 'crypto'
 const getverificationLink=()=>{
     const token = crypto.randomBytes(32).toString('hex')
     const expiresAt = new Date(Date.now() + 1000 * 60 * 60 )
-    const verificationLink=`http://localhost:5173/verify-email?token=${token}`;
+    const verificationLink=`${process.env.FRONTENDURL}/verify-email?token=${token}`;
     return {verificationLink,expiresAt,token}
 }
 export default getverificationLink;
