0c0aec564fe8e8bc77ff537590b411b07b63874a
Prine7643 <princekumar809016@gmail.com>
2025-11-09 17:12:28 +0530

    Frontend changes landingpages

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -26,7 +26,7 @@ const App = () => {
   // Register service worker once
   useEffect(() => {
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('../firebaseMessage.ts')
+      navigator.serviceWorker.register('/firebase-messaging-sw.js')
       .then((reg)=>console.log('service worker registered',reg.scope))
       .catch((err)=>console.log('service worker not registered',err))
     }
